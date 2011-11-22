1ff1426685ff62a01734323d9f650ee4388c4627
Rikard Erlandsson <rikard.erlandsson@ki.se>
2011-11-22 16:06:16 +0100

    testing command line
    [git-p4: depot-paths = "//depot/": change = 119]

---
+++ rikard_two/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            // The Length property provides the number of array elements
+            System.Console.WriteLine("parameter count = {0}", args.Length);
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                System.Console.WriteLine("Arg[{0}] = [{1}]", i, args[i]);
+            }
+
+            foreach (string s in args)
+            {
+                
+                System.Console.Write(s);
+
+                Console.WriteLine(System.IO.File.Exists(s) ? " - File exists." : " - File does not exist.");
+
+                if (File.Exists(s))
+                {
+
+
+                }
+
+            }
+
+
+
+
+
+            return (0);
+        }
+
+    }
+}

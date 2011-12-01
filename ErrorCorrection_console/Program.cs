a79804ac3f014ee5ebe7437467b22f91e4e9fd62
Indranil Sinha <indranil.sinha.2@ki.se>
2011-12-01 12:09:17 +0100

    corrected
    [git-p4: depot-paths = "//depot/": change = 148]

--- ErrorCorrection_console/Program.cs
+++ ErrorCorrection_console/Program.cs
@@ -10,23 +10,47 @@ namespace ErrorCorrection_console
     {
         static void Main(string[] args)
         {
-            ErrorHandler ErrH = new ErrorHandler();
-            var p = new Options() {
+            Console.WriteLine("For error handler press 1 and for fastq record count press 2 followed by enter=");
+            if (Console.ReadLine()=="1")
+            {
+                ErrorHandler ErrH = new ErrorHandler();
+                var p = new Options() {
                 { "seq=", "Select the FasQ file with error ", (v) => ErrH.refSeqFilepath = v },
                 { "Err=", "Select the error output file", (v) => ErrH.errorFilepath = v },
                 { "Ecor=", "Select the error Corrected file", (v) => ErrH.errorCorrectedFilepath = v },
             };
-            foreach (var x in p.Parse(args)) ;
-            if (ErrH.refSeqFilepath == null || ErrH.errorFilepath== null )
-            {
-                Console.WriteLine("Usage: mono ErrorCorrection_console.exe [OPTIONS]");
-                Console.WriteLine();
-                p.WriteOptionDescriptions(Console.Out);
-                return;
-            }
+                foreach (var x in p.Parse(args)) ;
+                if (ErrH.refSeqFilepath == null || ErrH.errorFilepath == null)
+                {
+                    Console.WriteLine("Usage: mono ErrorCorrection_console.exe [OPTIONS]");
+                    Console.WriteLine();
+                    p.WriteOptionDescriptions(Console.Out);
+                    return;
+                }
 
-            Console.WriteLine("Junction Lables Simulator Error Correction");
-            ErrH.ErrorCorrection();
+                Console.WriteLine("Junction Lables Simulator Error Correction");
+                ErrH.ErrorCorrection();  
+            }
+            else if (Console.ReadLine()=="2")
+            {
+                recordCount rc = new recordCount();
+                var p = new Options() {
+                { "seq=", "Select the FasQ file with error ", (v) => rc.refSeqFilepath = v },
+                };
+                foreach (var x in p.Parse(args)) ;
+                if (rc.refSeqFilepath == null)
+                {
+                    Console.WriteLine("Usage: mono ErrorCorrection_console.exe [OPTIONS]");
+                    Console.WriteLine();
+                    p.WriteOptionDescriptions(Console.Out);
+                    return;
+                }
+
+                Console.WriteLine("Junction Lables Simulator Record Count");
+                rc.Count();
+
+            }
+            
         }
     }
 }
---
+++ ErrorCorrection_console/recordCount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Linnarsson.Dna;
+using Linnarsson.Utilities;
+using Linnarsson.Mathematics;
+using System.IO;
+
+namespace ErrorCorrection_console
+{
+    public class recordCount
+    {
+        public string refSeqFilepath { get; set; }
+        public recordCount()
+        { }
+        public void Count()
+        {
+            FastQFile fq = FastQFile.Load(refSeqFilepath, 64);
+            Console.WriteLine (fq.Records.Count().ToString());
+        }
+    }
+}

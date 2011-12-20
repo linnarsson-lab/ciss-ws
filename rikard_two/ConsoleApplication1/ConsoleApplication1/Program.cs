f21a39203a62557f597b2b4d56a74e3b7e61840f
Rikard Erlandsson <rikard.erlandsson@ki.se>
2011-12-20 16:07:50 +0100

    playing a little
    [git-p4: depot-paths = "//depot/": change = 211]

--- rikard_two/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ rikard_two/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -20,25 +20,52 @@ namespace ConsoleApplication1
 
             foreach (string s in args)
             {
-                
                 System.Console.Write(s);
-
                 Console.WriteLine(System.IO.File.Exists(s) ? " - File exists." : " - File does not exist.");
-
                 if (File.Exists(s))
                 {
-
-
+                    string[] lines = System.IO.File.ReadAllLines(@s);
                 }
-
             }
 
-
-
-
+            string[] array2 = Directory.GetFiles(@"/media/ext5tb1/runs/111111_SN893_0092_BD036JACXX/Aligned.new.20111208/variantDetection", "P*_*_*");
+            foreach (string s in array2)
+            {
+                System.Console.Write(s + Environment.NewLine);
+            }
 
             return (0);
         }
 
+        private static void TraverseDirs(DirectoryInfo dir, string Pattern)
+        {
+            // Subdirs
+            try         // Avoid errors such as "Access Denied"
+            {
+                foreach (DirectoryInfo iInfo in dir.GetDirectories())
+                {
+                    if (iInfo.Name.StartsWith(Pattern))
+                        Console.WriteLine("Found dir:  " + iInfo.FullName);
+
+                    TraverseDirs(iInfo, Pattern);
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            // Subfiles
+            try         // Avoid errors such as "Access Denied"
+            {
+                foreach (FileInfo iInfo in dir.GetFiles())
+                {
+                    if (iInfo.Name.StartsWith(Pattern))
+                        Console.WriteLine("Found file: " + iInfo.FullName);
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
     }
 }

d2a3bf031e46782996f89197063e023e643ef908
PeterLonnerberg <peter.lonnerberg@ki.se>
2012-01-04 12:01:30 +0100

    Fixed error handling on mail and scp problem
    [git-p4: depot-paths = "//depot/": change = 262]

--- BkgFastQMailer/ReadMailer.cs
+++ BkgFastQMailer/ReadMailer.cs
@@ -28,7 +28,6 @@ namespace BkgFastQMailer
             string[] readsFiles = Directory.GetFiles(readsFolder);
             foreach (string email in mds.Keys)
             {
-                string allStatus = "sent";
                 Dictionary<string, string> resultByTaskId = new Dictionary<string, string>();
                 List<string> links = new List<string>();
                 foreach (MailTaskDescription md in mds[email])
@@ -42,21 +41,20 @@ namespace BkgFastQMailer
                         if (m.Success)
                         {
                             new ProjectDB().UpdateMailTaskStatus(md.id, "processing");
-                            string url = PublishReadsForDownload(readsFile);
-                            if (url == "")
+                            try
                             {
-                                logWriter.WriteLine(DateTime.Now.ToString() + " *** ERROR: Could not publish " + readsFile + " on www server!");
+                                string url = PublishReadsForDownload(readsFile);
+                                links.Add(url);
+                                status = "sent";
+                            }
+                            catch (Exception e)
+                            {
+                                logWriter.WriteLine(DateTime.Now.ToString() + " *** ERROR: Could not publish " + readsFile + " on www server: " + e);
                                 logWriter.Flush();
                                 status = "failed";
                             }
-                            else
-                            {
-                                links.Add(url);
-                                status = "copied";
-                            }
                         }
                     }
-                    if (status != "copied") allStatus = "";
                     if (status == "filemissing")
                     {
                         logWriter.WriteLine(DateTime.Now.ToString() + " *** ERROR: Can not locate a fq file for run=" + md.runNo + " and lane=" + md.laneNo);
@@ -64,6 +62,7 @@ namespace BkgFastQMailer
                     }
                     resultByTaskId[md.id] = status;
                 }
+                string allStatus = "sent";
                 try
                 {
                     if (links.Count > 0)
@@ -77,7 +76,7 @@ namespace BkgFastQMailer
                 }
                 foreach (string id in resultByTaskId.Keys)
                 {
-                    string status = (allStatus != "")? allStatus : resultByTaskId[id];
+                    string status = (allStatus != "sent")? allStatus : resultByTaskId[id];
                     new ProjectDB().UpdateMailTaskStatus(id, status);
                 }
             }
@@ -90,23 +89,24 @@ namespace BkgFastQMailer
             if (File.Exists(tempFqPath))
                 File.Delete(tempFqPath);
             File.Copy(readsFile, tempFqPath);
-            int gzipResult = CmdCaller.Run("gzip", tempFqPath);
-            if (gzipResult == 0)
-            {
-                File.Delete(tempFqPath);
-                string tempGzPath = tempFqPath + ".gz";
-                string destFilename = Path.GetFileName(readsFile) + ".gz";
-                string destPath = Path.Combine(Props.props.ResultDownloadUrl, destFilename);
-                string scpArg = string.Format("-P 9952 {0} {1}", tempGzPath, destPath);
-                Console.WriteLine(DateTime.Now.ToString() + ": scp " + scpArg);
-                int scpResult = CmdCaller.Run("scp", scpArg);
-                if (scpResult == 0)
-                {
-                    readsFileLink = Props.props.ResultDownloadFolderHttp + destFilename;
-                }
-                File.Delete(tempGzPath);
-            }
-                return readsFileLink;
+            CmdCaller cmd = new CmdCaller("gzip", tempFqPath);
+            if (cmd.ExitCode != 0)
+                throw new IOException("Could not call OS 'gzip' command: " + cmd.StdError);
+            File.Delete(tempFqPath);
+            string tempGzPath = tempFqPath + ".gz";
+            string destFilename = Path.GetFileName(readsFile) + ".gz";
+            string destPath = Path.Combine(Props.props.ResultDownloadUrl, destFilename);
+            string scpArg = string.Format("-P 9952 {0} {1}", tempGzPath, destPath);
+            Console.WriteLine(DateTime.Now.ToString() + ": scp " + scpArg);
+            CmdCaller scpCmd = new CmdCaller("scp", scpArg);
+            if (scpCmd.ExitCode != 0)
+                throw new IOException("Could not call OS 'scp' command: " + scpCmd.StdError);
+            readsFileLink = Props.props.ResultDownloadFolderHttp + destFilename;
+            CmdCaller chmodCmd = new CmdCaller("chmod", "u+w " + tempGzPath);
+            if (chmodCmd.ExitCode != 0)
+                throw new IOException("Could not call OS 'chmod' command: " + chmodCmd.StdError);
+            File.Delete(tempGzPath);
+            return readsFileLink;
         }
 
         private static void NotifyClient(string email, List<string> links)

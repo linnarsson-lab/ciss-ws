9eb8f0a4f3b974456d9539ff1f26d872d9e56513
Sten Linnarsson <sten.linnarsson@ki.se>
2012-01-11 17:00:34 +0100

    Tester for molecule counter
    [git-p4: depot-paths = "//depot/": change = 275]

---
+++ Sten_Linnarsson/MoleculeCountertester/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Linnarsson.Strt;
+
+namespace MoleculeCountertester
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Random rnd = new Random();
+			foreach (var m in new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 18, 25, 50, 75, 100, 150, 200 })
+			{
+				var mc = MoleculeCounter.Simulate(m, 256, 0.2, 10, 0.9, 10);
+				var p1 = mc.PosteriorMode();
+				var p2 = mc.PosteriorMean();
+				Console.WriteLine((m*(1+rnd.NextDouble()/10-0.05)) + "\t" + p1 + "\t" + p2);
+			}
+		}
+	}
+}

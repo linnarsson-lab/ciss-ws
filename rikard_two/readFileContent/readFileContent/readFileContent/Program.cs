dd8cb0d3dfac5f4dc511d24617011bfc9c7bf157
Rikard Erlandsson <rikard.erlandsson@ki.se>
2012-01-12 09:37:46 +0100

    for reading and counting reads pesent in a gff file
    [git-p4: depot-paths = "//depot/": change = 276]

--- rikard_two/readFileContent/readFileContent/readFileContent/Program.cs
+++ rikard_two/readFileContent/readFileContent/readFileContent/Program.cs
@@ -4,21 +4,121 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using Linnarsson.Dna;
+using System.Diagnostics;
 
 namespace readFileContent
 {
+
+    public class Fragment
+    {
+        public string seqname { get; set; }
+        public string source { get; set; }
+        public string feature { get; set; }
+        public long start { get; set; }
+        public long end { get; set; }
+        public string score { get; set; }
+        public char strand { get; set; }
+        public int frame { get; set; }
+        public string attribute { get; set; }
+
+        public Fragment(string Seqname, string Source, string Feature, long Start, long End, string Score,
+                       char Strand, int Frame, string Attribute)
+        {
+            seqname = Seqname;
+            source = Source;
+            feature = Feature;
+            start = Start;
+            end = End;
+            score = Score;
+            strand = Strand;
+            frame = Frame;
+            attribute = Attribute;
+        }
+    }
+
+    public class GFFfile : Dictionary<string, Fragment> 
+    {
+        public string filepath { get; set; }
+        public int fragmentcount;
+
+        public GFFfile(string Filepath)
+        {
+            filepath = Filepath;
+            fragmentcount = 0;
+            using (StreamReader r = new StreamReader(filepath))
+            {
+                string line;
+                while ((line = r.ReadLine()) != null)
+                {
+                    if (line.Length < 5) continue;
+                    if (line.Substring(0, 1) != "#")
+                    {
+                        string[] entries = line.Split(new Char[] { '\t' });
+                        if (entries.Length > 4)
+                        {
+                            Fragment temp = new Fragment(entries[0], entries[1], entries[2], Int32.Parse(entries[3]), Int32.Parse(entries[4]),
+                                 entries[5], char.Parse(entries[6]), Int32.Parse(entries[7]), entries[8]);
+                            this.addfragment(entries[0] + entries[3], temp);
+                            fragmentcount++;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void addfragment(string frag, Fragment Frg)
+        {
+            this.Add(frag, Frg);
+            fragmentcount++;
+        }
+
+        public void removefragment(string frag)
+        {
+            this.Remove(frag);
+            fragmentcount--;
+        }
+
+    }
+
+
+    public class chromosomeparts : Dictionary<string, scanpart>
+    {
+        public string chromo { get; set; }
+        public int partcount { get; set; }
+
+        public chromosomeparts(string CHR)
+        {
+            chromo = CHR;
+            partcount = 0;
+        }
+    }
+
+    public class scanpart
+    {
+        public int beginning { get; set; }
+        public int ending { get; set; }
+
+        public scanpart(int BEG, int END)
+        {
+            beginning = BEG;
+            ending = END;
+        }
+
+    }
+
     class Program
     {
+
         static int Main(string[] args)
         {
             if (args.Length != 1)
             {
                 usage();
-                return(1);
+                return (1);
             }
 
             string path = args[0];
-
+            GFFfile Design = new GFFfile("/media/ext5tb1/runs/111111_SN893_0092_BD036JACXX/Design-Report-id-488-1314174777.gff");
             if (Directory.Exists(path))
             {
                 Console.WriteLine(Environment.NewLine + "\tThe '" + path + "' is set as root.");
@@ -26,7 +126,7 @@ namespace readFileContent
             else
             {
                 Console.WriteLine(Environment.NewLine + "\tFolder '" + path + "' not found!");
-                return(2);
+                return (2);
             }
 
             string[] TheBams = new string[0];
@@ -55,47 +155,59 @@ namespace readFileContent
             }
 
             Console.WriteLine();
+            DateTime now = DateTime.Now;
+            Process proc = Process.GetCurrentProcess();
+            Console.WriteLine("\tStarting " + now + " memory allocated " + proc.PrivateMemorySize64);
+            Console.WriteLine();
+
+            List<string> chromos = new List<string>();
+            foreach (KeyValuePair<string, Fragment> kvm in Design)
+            {
+                if (chromos.IndexOf(kvm.Value.seqname) == -1)
+                {
+                    chromos.Add(kvm.Value.seqname);
+                chromosomeparts temp = new chromosomeparts();
+                Console.WriteLine("{0}, {1}, {2}, {3}", kvm.Key, kvm.Value.seqname, kvm.Value.start, kvm.Value.end);
+
+            }
+
 
             foreach (string bFile in TheBams)
             {
                 Console.WriteLine("\t" + bFile);
                 BamFile BF = new BamFile(bFile);
+
                 for (int i = 0; i < BF.Chromosomes.Length; i++)
                 {
-                    List<BamAlignedRead> MyList = BF.Fetch(BF.Chromosomes[i], 1, 5000000);
+                    Dictionary<int, int> onposcounter = new Dictionary<int, int>();
+                    List<BamAlignedRead> MyList = BF.Fetch(BF.Chromosomes[i], 1, BF.ChromosomeLengths[i]);
                     Console.WriteLine("\t\t" + BF.Chromosomes[i] + " - " + BF.ChromosomeLengths[i] + " - " + MyList.Count);
-                    string outputL = "";
                     if (!(MyList.Count < 1))
                     {
                         for (int ii = 0; ii < MyList.Count; ii++)
                         {
-                            if (MyList[ii].ExtraFields.Length != 0) 
-                            {
-                                outputL += "\t\t" + i;
-                                for (int iii = 0; iii < MyList[ii].ExtraFields.Length; iii++)
-                    			{
-                                    outputL += " " + iii + MyList[ii].ExtraFields[iii];
-			                    }
-                                outputL += " >CIGAR->" + MyList[ii].Cigar + Environment.NewLine;                            }
+                            if (onposcounter.ContainsKey(MyList[ii].Position))
+                                onposcounter[MyList[ii].Position]++;
+                            else
+                                onposcounter.Add(MyList[ii].Position, 1);
+                            //                  Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}", i, BF.Chromosomes[i], , MyList[ii].MappingQuality, 
+                            //                                                     MyList[ii].Strand, MyList[ii].IsMatePair, MyList[ii].MateStrand);
+
                         }
-                     }
-                    if (outputL.Length == 0)
-                    {
-                        Console.WriteLine("\t\tno mapped reads in interval OR no extra lines");
-                    } 
-                    else
-                    {
-                        Console.WriteLine(outputL);
                     }
 
-
+                    foreach (KeyValuePair<int, int> kvp in onposcounter)
+                        Console.WriteLine("{0}, {1,10:G}, {2,10:G}", BF.Chromosomes[i], kvp.Key, kvp.Value);
+                    onposcounter.Clear();
+                    MyList.Clear();
                 }
+
                 Console.WriteLine();
             }
 
 
             Console.WriteLine();
-            return(0);
+            return (0);
         }
 
         static void usage()

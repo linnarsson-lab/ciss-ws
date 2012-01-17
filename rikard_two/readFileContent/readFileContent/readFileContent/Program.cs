87258ca316ed162001b75954e91afa254d01d4bb
Rikard Erlandsson <rikard.erlandsson@ki.se>
2012-01-17 16:22:47 +0100

    rearranging
    [git-p4: depot-paths = "//depot/": change = 295]

--- rikard_two/readFileContent/readFileContent/readFileContent/Program.cs
+++ rikard_two/readFileContent/readFileContent/readFileContent/Program.cs
@@ -160,76 +160,55 @@ namespace readFileContent
             Console.WriteLine("\tStarting " + now + " memory allocated " + proc.PrivateMemorySize64);
             Console.WriteLine();
 
-  
-/*            List<string> chromos = new List<string>();
-            foreach (KeyValuePair<string, Fragment> kvm in Design)
-            {
-                if (chromos.IndexOf(kvm.Value.seqname) == -1)
-                {
-                    chromos.Add(kvm.Value.seqname);
-                chromosomeparts temp = new chromosomeparts();
-                Console.WriteLine("{0}, {1}, {2}, {3}", kvm.Key, kvm.Value.seqname, kvm.Value.start, kvm.Value.end);
-
-            }
-*/
-            // start to compare the projects, designate one project as reference and one project to compare
             TextWriter tw = new StreamWriter("readCount.txt");
 
             foreach (string bFile in TheBams)
             {
                 Console.WriteLine("\t" + bFile);
                 BamFile BF = new BamFile(bFile);
+                tw.WriteLine(bFile);
                 tw.WriteLine(DateTime.Now);
-                //                for (int i = 0; i < BF.Chromosomes.Length; i++)
-//                {
-                Dictionary<string, int> onposcounter = new Dictionary<string, int>();
-                List<string>   posexist = new List<string>();
-                //                    Console.WriteLine(BF.Chromosomes[i] + " - " + BF.ChromosomeLengths[i]);
+//                Dictionary<string, int> onposcounter = new Dictionary<string, int>();
+//                List<string>   posexist = new List<string>();
                 foreach (KeyValuePair<string, Fragment> KVP in Design)
                 {
-                    //                        string[] CHbam = BF.Chromosomes[i].Split('.');
                     string[] CHgff = KVP.Value.seqname.Split('r');
                     string CHROM = CHgff[1] + ".fa";
-                    int strt = KVP.Value.start - 250;
-                    if (strt < 1)
-                        strt = 1;
-                    int ende = KVP.Value.end + 250;
-                    //                        if (ende > BF.ChromosomeLengths[i])
-                    //                            ende = BF.ChromosomeLengths[i];
-                    List<BamAlignedRead> MyList = BF.Fetch(CHROM, strt, ende);
-                    //                        Console.WriteLine("\t\t" + BF.Chromosomes[i] + " - " + BF.ChromosomeLengths[i] + " - " + MyList.Count);
-                    List<string> tmp = new List<string>();
-                    if (!(MyList.Count < 1))
-                    {
-
-                        for (int ii = 0; ii < MyList.Count; ii++)
-                        {
-                            if (posexist.Contains(CHROM + MyList[ii].Position)) continue;
-                            if (onposcounter.ContainsKey(CHROM + MyList[ii].Position))
-                                onposcounter[CHROM + MyList[ii].Position]++;
-                            else
-                            {
-                                tmp.Add(CHROM + MyList[ii].Position);
-                                onposcounter.Add(CHROM + MyList[ii].Position, 1);
-                            }
-                            //                  Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}", i, BF.Chromosomes[i], , MyList[ii].MappingQuality, 
-                            //                                                     MyList[ii].Strand, MyList[ii].IsMatePair, MyList[ii].MateStrand);
-                        }
-                    }
-                    foreach (string str in tmp)
-                        posexist.Add(str);
-                    tmp.Clear();
-                    Console.SetCursorPosition(0, Console.CursorTop);
-                    Console.Write(" " + CHROM + " " + KVP.Key + " " + MyList.Count);
+//                    int strt = KVP.Value.start - 250;
+//                    if (strt < 1)
+//                        strt = 1;
+//                    int ende = KVP.Value.end + 250;
+                    List<BamAlignedRead> MyList = BF.Fetch(CHROM, KVP.Value.start, KVP.Value.end);
+//                    List<string> tmp = new List<string>();
+//                    if (!(MyList.Count < 1))
+//                    {
+//
+//                        for (int ii = 0; ii < MyList.Count; ii++)
+//                        {
+//                            if (posexist.Contains(CHROM + MyList[ii].Position)) continue;
+//                            if (onposcounter.ContainsKey(CHROM + MyList[ii].Position))
+//                                onposcounter[CHROM + MyList[ii].Position]++;
+//                            else
+//                            {
+//                                tmp.Add(CHROM + MyList[ii].Position);
+//                                onposcounter.Add(CHROM + MyList[ii].Position, 1);
+//                            }
+//                        }
+                    //                    }
+                    //                    foreach (string str in tmp)
+                    //                        posexist.Add(str);
+//                    tmp.Clear();
+//                    Console.SetCursorPosition(0, Console.CursorTop);
+//                    Console.Write(" " + CHROM + " " + KVP.Key + " " + MyList.Count);
+                    tw.WriteLine("{0,4:G} {1,10:G} {2,10:G} {3,10:G}", CHROM, KVP.Value.start, KVP.Value.end, MyList.Count);
                     MyList.Clear();
                 }
+//                foreach (KeyValuePair<string, int> kvp in onposcounter)
+//                {
+//                    string[] genomepos = kvp.Key.Split('.');
+//                    tw.WriteLine("{0,2:G} {1,10:G} {2,10:G}", genomepos[0], kvp.Key, kvp.Value);
 //                }
-                foreach (KeyValuePair<string, int> kvp in onposcounter)
-                {
-                    string[] genomepos = kvp.Key.Split('.');
-                    tw.WriteLine("{0,2:G} {1,10:G} {2,10:G}", genomepos[0], kvp.Key, kvp.Value);
-                }
-                onposcounter.Clear();
+//                onposcounter.Clear();
                 Console.WriteLine();
             }
             tw.Close();

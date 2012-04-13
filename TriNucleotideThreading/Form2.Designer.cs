690663f947d75a5b1f166313e44bbd9a5fdccaf7
Indranil Sinha <indranil.sinha.2@ki.se>
2012-04-13 15:03:25 +0100

    some sequence features added
    [git-p4: depot-paths = "//depot/": change = 381]

--- TriNucleotideThreading/Form2.Designer.cs
+++ TriNucleotideThreading/Form2.Designer.cs
@@ -33,6 +33,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.StarttextBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -79,11 +81,33 @@
             this.StarttextBox2.TabIndex = 4;
             this.StarttextBox2.Text = "0";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(34, 205);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 40);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "one fasta file to multiple fasta file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(34, 277);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(164, 35);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "GenBank to FASTA";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(261, 234);
+            this.ClientSize = new System.Drawing.Size(368, 355);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.StarttextBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -103,5 +127,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox StarttextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- TriNucleotideThreading/Form2.cs
+++ TriNucleotideThreading/Form2.cs
@@ -38,5 +38,71 @@ namespace TriNucleotideThreading
             output.Close();
             MessageBox.Show("End of Run!!"); 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select a fasta file to read";
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+            string[] lines = System.IO.File.ReadAllLines(ofd.FileName);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i].StartsWith(">"))
+                {
+                    //MessageBox.Show(lines[i].ToString());
+                    string filename = lines[i].TrimStart('>').ToString();
+                    //MessageBox.Show(filename);
+                    var output = (Path.Combine(Path.GetDirectoryName(ofd.FileName),  filename + ".fa")).OpenWrite();
+                    output.WriteLine(lines[i].ToString());
+                    i++;
+                    do
+                    {
+                        output.WriteLine(lines[i].ToString());
+                        i++;
+                        if (i >= lines.Length) break;
+                    } while (!lines[i].StartsWith(">"));
+                    output.Close();
+                    i--;
+
+                }
+            }
+            MessageBox.Show("end of run!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select a GenBank file to read";
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+            string[] gbkFilenameLine = System.IO.File.ReadAllLines(ofd.FileName);
+            var refFlatoutput = (Path.Combine(Path.GetDirectoryName(ofd.FileName)) + '\\' + "refFlat" + ".txt").OpenWrite();
+            char strand='+';
+            for (int i = 0; i < gbkFilenameLine.Length; i++)
+            {
+                string[] gbkFilename = gbkFilenameLine[i].Split('\t');
+                string fileNamePath = (Path.Combine(Path.GetDirectoryName(ofd.FileName)) +'\\' + gbkFilename[7]);
+                //MessageBox.Show(fileNamePath);
+                GenbankFile gf = GenbankFile.Load(fileNamePath);
+                GenbankRecord gr = gf.Records[0];
+                GenbankFeature feature = gr.Features[0];
+                if (feature.Strand.ToString()!="Forward")
+                {
+                    strand='-';
+                }
+                
+                var output = (Path.Combine(Path.GetDirectoryName(ofd.FileName)) +'\\' + "fastaFiles" +'\\' + gr.LocusName.ToString() + ".fa").OpenWrite();
+                output.WriteLine(">" + gr.LocusName.ToString());
+                output.WriteLine(gr.Sequence.ToString());
+                output.Close();
+                refFlatoutput.WriteLine(gr.LocusName.ToString() + '\t' + gr.Accession.ToString() + '\t' + gr.LocusName + '\t' + strand.ToString() + '\t' + "1" + '\t' + gr.SequenceLength.ToString() + '\t' + "1" + '\t' + gr.SequenceLength.ToString() + '\t' + "1" + '\t' + "1" + '\t' +gr.SequenceLength.ToString());
+                //MessageBox.Show(gr.LocusName.ToString() + " and " + gr.SequenceLength.ToString());
+                
+            }
+            refFlatoutput.Close();
+            MessageBox.Show("End of Run!!");
+            
+           
+
+        }
     }
 }

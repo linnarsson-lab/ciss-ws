50e6a40fc91e68419876793a21402d2f71c12b3c
PeterLonnerberg <peter.lonnerberg@ki.se>
2012-05-21 14:52:50 +0100

    x
    [git-p4: depot-paths = "//depot/": change = 422]

---
+++ CorrCell/CorrCell/DataSampler.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CorrCell
+{
+    /// <summary>
+    /// Used to sample mean expression values from an expression data derived distribution of counts.
+    /// </summary>
+    public class DataSampler
+    {
+        private Random rnd;
+        private int[] countIvlStarts;
+        private List<List<double>> meansByIvl = new List<List<double>>();
+
+        /// <summary>
+        /// Setup for sampling
+        /// </summary>
+        /// <param name="expression">Input expression data</param>
+        /// <param name="minValuesPerIvl">Minimum number of values in each bin to sample from</param>
+        public DataSampler(Expression expression, int minValuesPerIvl)
+        {
+            rnd = new Random(DateTime.Now.Millisecond);
+            Dictionary<int, List<double>> meansByCount = SortMeansByCount(expression);
+            DefineIntervalsOfMeans(minValuesPerIvl, meansByCount);
+        }
+
+        /// <summary>
+        /// Sample a mean expression value for a given count
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns>An expression value</returns>
+        public double Sample(int count)
+        {
+            if (count < countIvlStarts[0])
+                throw new ArgumentOutOfRangeException("Can not sample data at count < " + countIvlStarts[0].ToString() + " Input: " + count.ToString());
+            int ivlIdx = Array.BinarySearch(countIvlStarts, count);
+            if (ivlIdx < 0)
+                ivlIdx = ~ivlIdx - 1;
+            List<double> ivlData = meansByIvl[ivlIdx];
+            return ivlData[rnd.Next(ivlData.Count)];
+        }
+
+        private void DefineIntervalsOfMeans(int minValuesPerIvl, Dictionary<int, List<double>> meansByCount)
+        {
+            List<double> ivlMeans = new List<double>();
+            int[] counts = meansByCount.Keys.ToArray();
+            Array.Sort(counts);
+            List<int> TempIvlStarts = new List<int>();
+            int ivlNValues = 0;
+            int ivlStartIdx = 0;
+            for (int i = 0; i < counts.Length; i++)
+            {
+                int count = counts[i];
+                ivlMeans.AddRange(meansByCount[count]);
+                ivlNValues += meansByCount[count].Count;
+                if (ivlNValues >= minValuesPerIvl)
+                {
+                    meansByIvl.Add(ivlMeans);
+                    TempIvlStarts.Add(counts[ivlStartIdx]);
+                    ivlMeans = new List<double>();
+                    ivlNValues = 0;
+                    ivlStartIdx = i + 1;
+                }
+            }
+            if (ivlMeans.Count > 0)
+            {
+                meansByIvl.Add(ivlMeans);
+                TempIvlStarts.Add(counts[ivlStartIdx]);
+            }
+            countIvlStarts = TempIvlStarts.ToArray();
+        }
+
+        private static Dictionary<int, List<double>> SortMeansByCount(Expression expression)
+        {
+            Dictionary<int, List<double>> meansByCount = new Dictionary<int, List<double>>();
+            for (int geneIdx = 0; geneIdx < expression.GeneCount; geneIdx++)
+            {
+                double mean = expression.GeneMean(geneIdx);
+                foreach (int count in expression.IterGeneValues(geneIdx, false))
+                {
+                    List<double> meansAtCount;
+                    if (!meansByCount.TryGetValue(count, out meansAtCount))
+                        meansAtCount = new List<double>();
+                    meansAtCount.Add(mean);
+                }
+            }
+            return meansByCount;
+        }
+
+    }
+}
---
+++ CorrCell/CorrCell/Expression.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CorrCell
+{
+    /// <summary>
+    /// Represents expression values of transcripts for a number of cells
+    /// </summary>
+    public class Expression
+    {
+        private double[,] data;
+
+        /// <summary>
+        /// Number of genes in data the data set
+        /// </summary>
+        public int GeneCount { get { return data.GetLength(0); } }
+        /// <summary>
+        /// Number of cells in the data set
+        /// </summary>
+        public int CellCount { get { return data.GetLength(1); } }
+
+        /// <summary>
+        /// Fetch the expression value for a specific gene and cell
+        /// </summary>
+        /// <param name="geneIdx"></param>
+        /// <param name="cellIdx"></param>
+        /// <returns></returns>
+        public double GetValue(int geneIdx, int cellIdx)
+        {
+            return data[geneIdx, cellIdx];
+        }
+
+        /// <summary>
+        /// Get the average count for a specific gene
+        /// </summary>
+        /// <param name="geneIdx"></param>
+        /// <returns></returns>
+        public double GeneMean(int geneIdx)
+        {
+            int n = 0;
+            double sum = 0.0;
+            foreach (double value in IterGeneValues(geneIdx, false))
+            {
+                n++;
+                sum += value;
+            }
+            return sum / n;
+        }
+
+        /// <summary>
+        /// Get the average count for a specific cell
+        /// </summary>
+        /// <param name="cellIdx"></param>
+        /// <returns></returns>
+        public double CellMean(int cellIdx)
+        {
+            int n = 0;
+            double sum = 0.0;
+            foreach (double value in IterCellValues(cellIdx, false))
+            {
+                n++;
+                sum += value;
+            }
+            return sum / n;
+        }
+
+        /// <summary>
+        /// Iterate the counts for all cells of a specific gene
+        /// </summary>
+        /// <param name="geneIdx"></param>
+        /// <param name="includeNaN">If false, will only return values for cells where data exist</param>
+        /// <returns></returns>
+        public IEnumerable<double> IterGeneValues(int geneIdx, bool includeNaN)
+        {
+            for (int col = 0; col < GeneCount; col++)
+                if (!double.IsNaN(data[geneIdx, col]) || includeNaN)
+                    yield return data[geneIdx, col];
+        }
+
+        /// <summary>
+        /// Iterate the counts for all genes of a specific cell
+        /// </summary>
+        /// <param name="geneIdx"></param>
+        /// <param name="includeNaN">If false, will only return values for genes where data exist</param>
+        /// <returns></returns>
+        public IEnumerable<double> IterCellValues(int cellIdx, bool includeNaN)
+        {
+            for (int row = 0; row < CellCount; row++)
+                if (!double.IsNaN(data[row, cellIdx]) || includeNaN)
+                    yield return data[row, cellIdx];
+        }
+
+        /// <summary>
+        /// Iterate all counts in the whole table
+        /// </summary>
+        /// <param name="includeNaN">If false, will only return valid counts</param>
+        /// <returns></returns>
+        public IEnumerable<double> IterValues(bool includeNaN)
+        {
+            for (int row = 0; row < CellCount; row++)
+                for (int col = 0; col < GeneCount; col++)
+                    if (!double.IsNaN(data[row, col]) || includeNaN)
+                        yield return data[row, col];
+        }
+
+        /// <summary>
+        /// Init from a expression.tab STRT pipeline output file
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public static Expression FromExpressionFile(string file)
+        {
+            return new Expression();
+        }
+
+    }
+}

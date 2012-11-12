f2f16755847173f107f84dfb4559e366d5582057
PeterLonnerberg <peter.lonnerberg@ki.se>
2012-11-12 12:58:37 +0100

    x
    [git-p4: depot-paths = "//depot/": change = 622]

---
+++ HAC/SHAC/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HAC;
+
+namespace SHAC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length <= 1 || args[0].StartsWith("-h") || args[0] == "--help")
+            {
+                Console.WriteLine("Usage:\nSHAC -d DISTANCEMETHOD -l LINKAGEMETHOD -c NEIGHBORHOOD EXPRFILE");
+                Console.WriteLine("DISTANCEMETHOD is one of 'Euclidian', 'Manhattan', 'Chebyshev', 'Canberra', 'BrayCurtis'");
+                Console.WriteLine("LINKAGEMETHOD is one of 'Single', 'Complete', 'Average', 'Centroid'");
+                Console.WriteLine("NEIGHBORHOOD is one of 'Queen', 'Rook'");
+                Console.WriteLine("EXPRFILE is a table of data. The first and second lines have to contain\n" +
+                                  "   the X and Y coordinate, respectively, of each sample (sample 1 in column 2, etc.).");
+                return;
+            }
+        }
+    }
+}

2c9b37005fbb9c103ea7eb09bc6835a6e01451f0
PeterLonnerberg <peter.lonnerberg@ki.se>
2012-11-19 14:27:10 +0100

    x
    [git-p4: depot-paths = "//depot/": change = 641]

---
+++ HAC/HAC/Transformation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HAC
+{
+    public abstract class Transformation
+    {
+        public abstract void Transform(Element[] elements);
+
+        public static Transformation GetTransformation(string transformation)
+        {
+            switch (transformation.ToLower())
+            {
+                case "zscore":
+                    return new ZScoreTransform();
+                case "chisq":
+                    return new ChiSqTransform();
+                case "":
+                case null:
+                    return new NullTransformation();
+                default:
+                    throw new ArgumentException("Unknown transformation: " + transformation);
+            }
+        }
+    }
+}

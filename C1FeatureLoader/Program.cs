9b22c86a7a00e5edb8ad17b78815024b3db24dbb
PeterLonnerberg <peter.lonnerberg@ki.se>
2013-08-06 15:30:55 +0100

    refactored handling of 5'extension
    [git-p4: depot-paths = "//depot/": change = 761]

--- C1FeatureLoader/Program.cs
+++ C1FeatureLoader/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Linnarsson.Dna;
+using Linnarsson.Strt;
 
 namespace C1
 {
@@ -15,16 +16,20 @@ namespace C1
                 Console.WriteLine("Usage:\nmono C1FeatureLoader.exe GENOME\nwhere genome is e.g. 'mm10', 'mm10_aUCSC', 'hg19_sENSE'");
                 return;
             }
-            StrtGenome g = StrtGenome.GetGenome(args[0]);
-            string organism = g.Abbrev;
-            string trName = g.BuildVarAnnot;
+            StrtGenome genome = StrtGenome.GetGenome(args[0]);
+            string organism = genome.Abbrev;
+            string trName = genome.BuildVarAnnot;
             Props.props.DirectionalReads = true;
-            AnnotationReader annotationReader = AnnotationReader.GetAnnotationReader(g);
+            AnnotationReader annotationReader = AnnotationReader.GetAnnotationReader(genome);
             Console.WriteLine("Building gene models...");
-            annotationReader.BuildGeneModelsByChr();
+            int nModels = annotationReader.BuildGeneModelsByChr();
+            Console.WriteLine("...{0} gene models constructed.", nModels);
             Console.WriteLine("Extending 5' ends...");
-            annotationReader.Extend5PrimeEnds();
-            Transcriptome tt = new Transcriptome(null, g.BuildVarAnnot, g.Abbrev, g.Annotation, annotationReader.VisitedAnnotationPaths,
+            GeneFeature5PrimeModifier m = new GeneFeature5PrimeModifier();
+            annotationReader.AdjustGeneFeatures(m);
+            Console.WriteLine("...{0} genes had their 5' end extended, {1} with the maximal {2} bps.",
+                               m.nExtended, m.nFullyExtended5Primes, Props.props.GeneFeature5PrimeExtension);
+            Transcriptome tt = new Transcriptome(null, genome.BuildVarAnnot, genome.Abbrev, genome.Annotation, annotationReader.VisitedAnnotationPaths,
                                                  "", DateTime.Now, "1", DateTime.MinValue, null);
             Console.WriteLine("Inserting transcriptome metadata into database...");
             C1DB db = new C1DB();

8ec831708f812f24acf097c2bed87d1512b47cb5
PeterLonnerberg <peter.lonnerberg@ki.se>
2014-02-14 11:16:25 +0100

    added 3/5' extension
    [git-p4: depot-paths = "//depot/": change = 931]

--- PeakAnnotator/PeakAnnotator.cs
+++ PeakAnnotator/PeakAnnotator.cs
@@ -95,6 +95,8 @@ namespace PeakAnnotator
                         int start = int.Parse(m.Groups[2].Value) - 1; // Allow margin of one to handle 0/1 offset question
                         int end = int.Parse(m.Groups[3].Value) + 1;
                         char trStrand = m.Groups[4].Value[0];
+                        start -= (trStrand == '+') ? settings.ext5Prime : settings.ext3Prime;
+                        end += (trStrand == '+') ? settings.ext3Prime : settings.ext5Prime;
                         string name = m.Groups[5].Value;
                         name = MakeTSSNameSortableOnGeneName(name);
                         name = MakeTSSNameUnique(name);
--- PeakAnnotator/Program.cs
+++ PeakAnnotator/Program.cs
@@ -11,6 +11,8 @@ namespace PeakAnnotator
         public List<string> infiles = new List<string>();
         public string outfile = "repeat_expression.tab";
         public string genomeName = "Mm";
+        public int ext3Prime = 0;
+        public int ext5Prime = 0;
         public StrtGenome genome;
 
         public PeakAnnotatorSettings(string[] args)
@@ -19,6 +21,8 @@ namespace PeakAnnotator
             for (; argIdx < args.Length; argIdx++)
             {
                 if (args[argIdx] == "-g") genomeName = args[++argIdx];
+                else if (args[argIdx] == "-3") ext3Prime = int.Parse(args[++argIdx]);
+                else if (args[argIdx] == "-5") ext5Prime = int.Parse(args[++argIdx]);
                 else if (args[argIdx] == "-o") outfile = args[++argIdx];
                 else infiles.Add(args[argIdx]);
             }
@@ -32,10 +36,12 @@ namespace PeakAnnotator
         {
             if (args.Length == 0 || args[0] == "--help" || args[0] == "-h")
             {
-                Console.WriteLine("Usage:\nmono PeakAnnotator.exe -g GENOME -o OUTPUTFILE INFILE [INFILE2...]\n\n" +
+                Console.WriteLine("Usage:\nmono PeakAnnotator.exe [OPTIONS] -g GENOME -o OUTPUTFILE INFILE [INFILE2...]\n\n" +
                                   "N.B.: INFILEs are output files from Map2Pclu, e.g.: chr TAB strand TAB pos TAB count\n" +
                                   "      pos is where the read 5' end maps, i.e. if strand='-', pos is max of the aligned positions\n" +
                                   "Options:\n" +
+                                  "-3 N             Extend TSS region by N bases in the 3' end" +
+                                  "-5 N             Extend TSS region by N bases in the 5' end" +
                                   "-g GENOME        Genome to analyze, e.g. 'Mm' \n");
 
             }

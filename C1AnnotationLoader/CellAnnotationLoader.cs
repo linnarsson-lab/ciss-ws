42c7f1791f184e39992b16a7a679750ecd8342b1
PeterLonnerberg <peter.lonnerberg@ki.se>
2014-03-17 17:46:10 +0100

    x
    [git-p4: depot-paths = "//depot/": change = 938]

---
+++ C1AnnotationLoader/CellAnnotationLoader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using C1;
+
+namespace C1AnnotationLoader
+{
+    public class CellAnnotationLoader
+    {
+        public void Process(string tabFile)
+        {
+            C1DB db = new C1DB();
+            using (StreamReader reader = new StreamReader(tabFile))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.StartsWith("#"))
+                        continue;
+                    string[] fields = line.Split('\t');
+                    string chip = fields[0].Trim();
+                    string chipWell = fields[1].Trim();
+                    for (int i = 2; i < fields.Length; i += 2)
+                    {
+                        string key = fields[i].Trim();
+                        string value = fields[i + 1].Trim();
+                        try
+                        {
+                            Cell cell = db.GetCellFromChipWell(chip, chipWell);
+                            db.InsertOrUpdateCellAnnotation((int)cell.CellID, key, value);
+                        }
+                        catch (IndexOutOfRangeException)
+                        {
+                            Console.WriteLine("Can not find cell in DB: {0} {1}", chip, chipWell);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C1AnnotationLoader/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C1AnnotationLoader
+{
+    class C1AnnotationLoaderProgram
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 || args[0] == "--help" || args[0] == "-h")
+            {
+                Console.WriteLine("Usage:\nmono C1AnnotationLoader.exe ANNOTATIONTABFILE\n\n" +
+                                  "ANNOTATIONTABFILE should consist of lines of:\n" +
+                                  "Chip TAB ChipWell TAB AnnotName TAB AnnotValue [ AnnotName2 TAB AnnotValue2... ]");
+            }
+            else
+            {
+                int argIdx = 0;
+                string infile = args[argIdx];
+                new CellAnnotationLoader().Process(infile);
+            }
+        }
+    }
+}
